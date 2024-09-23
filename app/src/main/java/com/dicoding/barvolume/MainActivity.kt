4465e68f64bbbc62ffb4ee0fb46ad68509e98b22
Jordi Irawan <64052045+jrdrwn@users.noreply.github.com>
2024-09-23 23:27:45 +0700

    initial app
    Signed-off-by: Jordi Irawan <64052045+jrdrwn@users.noreply.github.com>

---
+++ app/src/main/java/com/dicoding/barvolume/MainActivity.kt
@@ -0,0 +1,81 @@
+package com.dicoding.barvolume
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var editWidth: EditText
+    private lateinit var editHeight: EditText
+    private lateinit var editLength: EditText
+    private lateinit var btnCalculate: Button
+    private lateinit var tvResult: TextView
+
+    companion object {
+        private const val STATE_RESULT = "state_result"
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        editWidth = findViewById(R.id.edit_width)
+        editHeight = findViewById(R.id.edit_height)
+        editLength = findViewById(R.id.edit_length)
+        btnCalculate = findViewById(R.id.btn_calculate)
+        tvResult = findViewById(R.id.tv_result)
+
+        btnCalculate.setOnClickListener(this)
+
+        if (savedInstanceState != null) {
+            val result = savedInstanceState.getString(STATE_RESULT)
+            tvResult.text = result
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        outState.putString(STATE_RESULT, tvResult.text.toString())
+    }
+
+    override fun onClick(v: View?) {
+        if (v?.id == R.id.btn_calculate) {
+            val inputLength = editLength.text.toString().trim()
+            val inputWidth = editWidth.text.toString().trim()
+            val inputHeight = editHeight.text.toString().trim()
+            var isEmptyFields = false
+            if (inputLength.isEmpty()) {
+                isEmptyFields = true
+                editLength.error = "Field ini tidak boleh kosong"
+            }
+            if (inputWidth.isEmpty()) {
+                isEmptyFields = true
+                editWidth.error = "Field ini tidak boleh kosong"
+            }
+            if (inputHeight.isEmpty()) {
+                isEmptyFields = true
+                editHeight.error = "Field ini tidak boleh kosong"
+            }
+
+            if (!isEmptyFields) {
+                val volume = inputLength.toDouble() * inputWidth.toDouble() * inputHeight.toDouble()
+                tvResult.text = volume.toString()
+            }
+        }
+    }
+}
